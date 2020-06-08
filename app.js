09bd7ae8ee07b5f94189d53e988a04fb82efa711
figueroar <raul.a.figueroa@hotmail.com>
2020-06-08 11:40:09 -0400

    proof of work

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 let Block = require('./block')
 let Blockchain = require('./blockchain')
-
 let Transaction = require('./transaction')
 
 let transaction = new Transaction("Mary", "Jerry", 100)
+
 let genesisBlock = new Block();
 let blockchain = new Blockchain(genesisBlock)
+
+let block = blockchain.getNextBlock([transaction])
+blockchain.addBlock(block)
 
 console.log(blockchain)
--- block.js
+++ block.js
@@ -1,20 +1,19 @@
 class Block {
   constructor() {
-    this.index = 0;
-    this.previousHash = "";
-    this.hash = "";
-    this.nonce = 0; 
-    this.transactions = []   
+    this.index = 0
+    this.previousHash = ""
+    this.hash = ""
+    this.nonce = 0
+    this.transactions = []
   }
 
   get key() {
-    return JSON.stringify(this.transactions) + this.index + this. previousHash + this.nonce
+    return JSON.stringify(this.transactions) + this.index + this.previousHash + this.nonce
   }
 
-  addtransation(transaction) {
-    this.transaction.push(transaction)
+  addTransaction(transaction) {
+    this.transactions.push(transaction)
   }
-
 
 };
 
