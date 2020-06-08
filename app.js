b4d019a2b1328eb79e1b838372d8b500ded6a709
figueroar <raul.a.figueroa@hotmail.com>
2020-06-08 19:08:09 -0400

    mining block / on local

--- app.js
+++ app.js
@@ -1,57 +1,74 @@
+// module(model) imports 
 let Block = require('./block')
 let Blockchain = require('./blockchain')
 let Transaction = require('./transaction')
 
+// npm install
 const express = require('express')
-const app = express()
 const bodyParser = require('body-parser')
 
-let transactions = []
+// launching express
+const app = express()
 
-//body
+
+// creating out blockcahin
+let transactions = []
+let genesisBlock = new Block()
+let blockchain = new Blockchain(genesisBlock)
+//abobe we have initialized a bloackchain
+
+//body 
 app.use(bodyParser.json())
-///
 
 app.get('/', function(req, res){
   res.send("hello world")
 })
 
-app.post("/transactions", function(req, res){
+app.get('/mine', function(req, res){
+  // we are mining blocks then returning a block
+  let block = blockchain.getNextBlock(transactions)
+  blockchain.addBlock(block)
+  res.json(block)
+})
 
+app.post("/transactions", function(req, res){
+  // in this router we will be posting all the trnsactions
   let to = req.body.to;
   let from = req.body.from;
   let amount = req.body.amount;
 
   // we will be adding the new transaction 
   let transaction = new Transaction(from, to, amount)
-  
   transactions.push(transaction)
-
-  res.json(transactions)
-
-  
+  res.json(transactions) // now we polulated transactionS
 })
 
 app.get('/blockchain', function(req, res){
-  
-
-  let transaction = new Transaction("Mary", "Jerry", 100)
-
-  let genesisBlock = new Block();
-  let blockchain = new Blockchain(genesisBlock)
-
-  let block = blockchain.getNextBlock([transaction])
-  blockchain.addBlock(block)
-
-  let anotherTransaction = new Transaction("Azam", "Jerry", 10)
-  let block1 = blockchain.getNextBlock([anotherTransaction, transaction])
-  blockchain. addBlock(block1)
-
+  // in this route, we then get the complete blockchain
   res.json(blockchain)
 })
 
+// our localhost is listening
 app.listen(3000, function(){
   console.log("server has started")
 })
 
 
+
+
+
+
+//Dummy data
+// let transaction = new Transaction("Mary", "Jerry", 100)
+
+  // let genesisBlock = new Block();
+  // let blockchain = new Blockchain(genesisBlock)
+
+  // let block = blockchain.getNextBlock([transaction])
+  // blockchain.addBlock(block)
+
+  // let anotherTransaction = new Transaction("Azam", "Jerry", 10)
+  // let block1 = blockchain.getNextBlock([anotherTransaction, transaction])
+  // blockchain. addBlock(block1)
+
+  // res.json(blockchain)
