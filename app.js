b4f4b251159cc8ec58fcc939fcb83e3b2411cb62
figueroar <raul.a.figueroa@hotmail.com>
2020-06-09 19:49:40 -0400

    new transaction model, new transactions not processing

--- app.js
+++ app.js
@@ -1,56 +1,64 @@
 
+// module(model) imports 
 let Block = require('./block')
 let Blockchain = require('./blockchain')
 let BlockchainNode = require('./BlockchainNode')
 let Transaction = require('./transaction')
 
-let fetch = require('node-fetch')
 
+// npm install  x --save
+let sha256 = require('js-sha256')
+let fetch = require('node-fetch')
 const express = require('express')
-const app = express()
 const bodyParser = require('body-parser')
+// launching express
+const app = express()
 
 let port = 3000
 
-// access the arguments
-process.argv.forEach(function(val,index,array){
+//acces the arguments
+process.argv.forEach(function(val,index, array){
+  //console.log(array)
   port = array[2]
 })
 
-if(port == undefined) {
-  port = 3000
+if (port == undefined) {
+  port = 3000;
 }
 
+
+// creating out blockcahin
 let transactions = []
 let nodes = []
 let genesisBlock = new Block()
 let blockchain = new Blockchain(genesisBlock)
+//abobe we have initialized a bloackchain
 
+//body 
 app.use(bodyParser.json())
 
-app.get('/resolve',function(req,res){
-
-  nodes.forEach(function(node){
-
-      fetch(node.url + '/blockchain')
-      .then(function(response){
-        return response.json()
-      })
-      .then(function(otherNodeBlockchain){
-
-          if(blockchain.blocks.length < otherNodeBlockchain.blocks.length) {
-            blockchain = otherNodeBlockchain
-          }
-
-          res.send(blockchain)
-
-      })
-
-  })
-
+app.get('/resolve', function(req,res){
+  console.log("/resolve route")
+  nodes.forEach( node => {
+    // console.log("/resolve forEach loop")
+    // console.log(node.url)
+    fetch("http://" + node.url + '/blockchain')
+    .then(function(response){
+      return response.json()
+    })
+    .then(function(otherNodeBlockchain){
+      if(blockchain.blocks.length < otherNodeBlockchain.blocks.length) {
+        blockchain = otherNodeBlockchain
+      }
+      res.send(blockchain)
+    })
+    // .catch(error => { console.error(error) 
+    // })
+  })  
 })
 
-app.post('/nodes/register',function(req,res){
+//noderegister multiplenodes
+app.post('/nodes/register', function(req, res){
 
   let nodesLists = req.body.urls
   nodesLists.forEach(function(nodeDictionary){
@@ -59,48 +67,46 @@ app.post('/nodes/register',function(req,res){
   })
 
   res.json(nodes)
-
+  console.log(nodes)
 })
 
-app.get('/nodes',function(req,res){
+
+app.get('/nodes', function(req, res){
   res.json(nodes)
 })
 
-app.get('/',function(req,res){
+app.get('/', function(req, res){
   res.send("hello world")
 })
 
-app.get('/mine',function(req,res){
+app.get('/mine', function(req, res){
+  // we are mining blocks then returning a block
+  let block = blockchain.getNextBlock(transactions)
+  blockchain.addBlock(block)
 
-    let block = blockchain.getNextBlock(transactions)
-    blockchain.addBlock(block)
-    transactions = []
-    console.log(transactions)
-    res.json(block)
+  //we reset the transactions // we dont want to add previous transactions tha are mined and completed
+  transactions = []
+  res.json(block)
 })
 
-app.post('/transactions',function(req,res){
+app.post("/transactions", function(req, res){
+  // in this router we will be posting all the trnsactions
+  let driverLicenseNumber = sha(req.body.driverLicenseNumber);
+  let violationDate = req.body.violationDate;
+  let violationType = req.body.violationType;
 
-  console.log(transactions)
-
-  let to = req.body.to
-  let from = req.body.from
-  let amount = req.body.amount
-
-  let transaction = new Transaction(from,to,amount)
-
+  // we will be adding the new transaction 
+  let transaction = new Transaction(driverLicenseNumber, violationDate, violationType)
   transactions.push(transaction)
-
-  res.json(transactions)
-
+  res.json(transactions) // now we polulated transactionS
 })
 
-app.get('/blockchain',function(req,res){
-
+app.get('/blockchain', function(req, res){
+  // in this route, we then get the complete blockchain
   res.json(blockchain)
-
 })
 
-app.listen(port,function(){
+// our localhost is listening
+app.listen(port, function(){
   console.log("server has started")
 })
--- transaction.js
+++ transaction.js
@@ -1,9 +1,11 @@
 
 class Transaction {
-  constructor(from,to,amount) {
-    this.from = from
-    this.to = to
-    this.amount = amount
+  constructor(driverLicenseNumber, violationDate, violationType) {
+    this.driverLicenseNumber = driverLicenseNumber;
+    this.violationDate = violationDate;
+    this.violationType = violationType;
+    this.noOfViolation = 1;
+    this.isDriverLicenseSuspended = false;
   }
 }
 
